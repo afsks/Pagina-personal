61e44f849df1d7c7a5b6a17ef910e41132797a38
Santiago Pedraza <Santiago.pedraza07@hotmail.com>
2020-08-28 19:45:17 -0500

    Adicion de js a la seleccion de imagenes, arreglo de imagen de fondo principal

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-var imagenes = ['../multimedia/certificados/DesarrolloWeb.jpg']
+const item = document.querySelector('.item');
+console.log(item)
+const iconCross = document.querySelector('.icon-cross');
+console.log(iconCross)
+const imgFull = document.querySelector('.item-background'); //constante para el contenedor de la imagen ampliada//
+console.log(imgFull);
+const programacionBasica = document.querySelector('#programacionBasica'); //constante para los contenedores de certificados sin ampliar//
+const desarrolloWeb = document.querySelector('#desarrolloWeb');
+const responsiveDesign = document.querySelector('#responsiveDesign');
+console.log(programacionBasica)
+
+
+
+
+item.addEventListener('click', hideShow)
+iconCross.addEventListener('click', hideShow)
+
+function hideShow(){
+    if(imgFull.classList.contains('is-active')){
+        imgFull.classList.remove('is-active')
+    }else{
+        imgFull.classList.add('is-active')
+    }
+}
